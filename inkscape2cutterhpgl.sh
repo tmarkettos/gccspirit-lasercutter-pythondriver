04a24fa48406f10dd59a3e29fa380e061c36a1d2
Theo Markettos <git+atm26@cl.cam.ac.uk>
2011-09-01 18:36:56 +0100

    Inkscape2hpgl script now uses my patched pstoedit to generate HPGL with the correct pens assigned to each colour
    (but not necessarily in the correct order)

--- inkscape2cutterhpgl.sh
+++ inkscape2cutterhpgl.sh
@@ -7,6 +7,8 @@
 # inkscape2cutterhpgl.sh somefile
 # will convert somefile.svg to somefile.eps to somefile.plt
 
+# requires my patched version of pstoedit that adds -pencolortable flag
+
 # currently set up for two colours (pens).  Scale factor of 1.4 seems to be
 # required to rescale after EPS conversion: exactly 1.4 or sqrt(2)?
 # Measured to be 1.40x and 1.41x
@@ -18,6 +20,6 @@ STEM=$1
 # hp2xx -c23456 scan+bracket-take1.libreoffice.pstoedit.plt
 
 inkscape --export-eps=$STEM.eps --export-area-drawing --export-text-to-path --without-gui $STEM.svg
-pstoedit -f "hpgl:-pencolors 2" -xscale 1.414 -yscale 1.414 $STEM.eps $STEM.plt
+pstoedit -f "hpgl:-pencolors 2 -pencolortable \"#000000,#ff0000,#00ff00,#ffff00,#0000ff,#ff00ff,#00ffff\"" -xscale 1.414 -yscale 1.414 $STEM.eps $STEM.plt
 hp2xx -c12 $STEM.plt &
 hpgl2cutter.py "Laser Settings/Card2mm.SGX" $STEM.plt $STEM.pcl
