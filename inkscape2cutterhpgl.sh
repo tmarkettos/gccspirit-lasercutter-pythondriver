a85a7e7235dfa28a9c87c72ec4b424e25b04362a
Theo Markettos <git+atm26@cl.cam.ac.uk>
2011-10-07 20:13:40 +0100

    inkscape2cutterhpgl: look for helper scripts relative to this script, allow specifying of material file and search local dir and Laser Settings dir

--- inkscape2cutterhpgl.sh
+++ inkscape2cutterhpgl.sh
@@ -13,15 +13,46 @@
 # required to rescale after EPS conversion: exactly 1.4 or sqrt(2)?
 # Measured to be 1.40x and 1.41x
 
+DEFAULT_MATERIAL=Card2mm.SGX
+
 STEM=$1
+if [[ -z $1 ]] ; then
+  echo "Syntax: inkscape2cutterhpgl.sh svgfile [material file]"
+  echo "(filename provided without extension)"
+  echo "material file also searched for in Laser Settings directory"
+  echo "Default material: $DEFAULT_MATERIAL"
+  exit
+fi
+
+if [[ ! -z $2 ]]; then
+  if [ -f $2 ]; then
+    MATERIAL_FILE=$2
+  else
+    MATERIAL_FILE="$( cd "$( dirname "$0" )/Laser Settings" && readlink -f $2 )"
+    if [[ ! -f $MATERIAL_FILE ]]; then
+      echo "Material file $MATERIAL_FILE not found (also searched Laser Settings directory)"
+      exit
+    fi
+  fi
+fi
+
+if [ -z "$MATERIAL_FILE" ]; then
+  MATERIAL_FILE="$( dirname "$0" )/Laser Settings/$DEFAULT_MATERIAL"
+fi
+
 
 # libreoffice draw, load Inkscape SVG, save as EPS
 # pstoedit -f "hpgl:-pencolors 2" scan+bracket-take1.eps scan+bracket-take1.libreoffice.pstoedit.plt
 # hp2xx -c23456 scan+bracket-take1.libreoffice.pstoedit.plt
 
+SCRIPT_PARENT="$( cd "$( dirname "$0" )" && pwd )"
+
+#echo $SCRIPT_PARENT
+echo "Using material $MATERIAL_FILE"
+#exit
 inkscape --export-eps=$STEM.eps --export-area-page --export-text-to-path --without-gui $STEM.svg
 pstoedit -f "hpgl:-pencolors 7 -pencolortable \"#000000,#ff0000,#00ff00,#ffff00,#0000ff,#ff00ff,#00ffff\"" -xscale 1.414 -yscale 1.414 $STEM.eps $STEM.plt
-tidyhpgl4cutter.py $STEM.plt $STEM.clean.plt
+$SCRIPT_PARENT/tidyhpgl4cutter.py $STEM.plt $STEM.clean.plt
 hp2xx -c1234 $STEM.clean.plt &
-hpgl2cutter.py "Laser Settings/OHPfilm-stack.SGX" $STEM.clean.plt $STEM.clean.pcl
+$SCRIPT_PARENT/hpgl2cutter.py "$MATERIAL_FILE" $STEM.clean.plt $STEM.clean.pcl
 #hpgl2cutter.py "Laser Settings/Arcmm.SGX" $STEM.plt $STEM.pcl
