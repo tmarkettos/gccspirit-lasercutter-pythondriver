26fe2fc23f9c18c5e60b1eba5154928e79d54ffa
Theo Markettos <git+atm26@cl.cam.ac.uk>
2012-01-31 19:19:08 +0000

    Tidying up laser cutter script to better state dependencies

--- inkscape2cutterhpgl.sh
+++ inkscape2cutterhpgl.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # convert inkscape files to laser-cutter suitable HPGL, using EPS as an
 # intermediate format.  Display the output in a GUI window using hp2xx
@@ -14,6 +14,9 @@
 # Measured to be 1.40x and 1.41x
 
 DEFAULT_MATERIAL=Card2mm.SGX
+PSTOEDIT=2pstoedit
+INKSCAPE=inkscape
+HP2XX=hp2xx
 
 STEM=$1
 if [[ -z $1 ]] ; then
@@ -40,12 +43,35 @@ if [ -z "$MATERIAL_FILE" ]; then
   MATERIAL_FILE="$( dirname "$0" )/Laser Settings/$DEFAULT_MATERIAL"
 fi
 
+hash $PSTOEDIT 2>&- || { echo >&2 \
+  "****** ERROR ******: pstoedit version 3.6 or later is required for conversion." \
+  "Download from http://www.pstoedit.net/ and build with libplot enabled"; exit 1; }
+
+
+PSTOEDIT_VERSION=`$PSTOEDIT --help 2>&1 | grep -m1 version | cut -c 19-22`
+PSTOEDIT_NEW_ENOUGH=$(echo $PSTOEDIT_VERSION " > 3.59" | bc)
+echo $PSTOEDIT_VERSION $PSTOEDIT_NEW_ENOUGH
+if [ $PSTOEDIT_NEW_ENOUGH -ne 1 ]; then
+  echo "****** WARNING ******"
+  echo "For correct laser power selection you need to install"
+  echo "pstoedit version 3.60 or later from"
+  echo "http://www.pstoedit.net/"
+  echo "with libplot enabled.  (Found version $PSTOEDIT_VERSION)."
+  echo "Continuing anyway..."
+  echo "***** END WARNING *****"
+fi
+
+
+hash $HP2XX 2>&- || { echo >&2 "****** WARNING ******: hp2xx is required for previewing plots - continuing anyway"; }
 
 # libreoffice draw, load Inkscape SVG, save as EPS
 # pstoedit -f "hpgl:-pencolors 2" scan+bracket-take1.eps scan+bracket-take1.libreoffice.pstoedit.plt
 # hp2xx -c23456 scan+bracket-take1.libreoffice.pstoedit.plt
 
 SCRIPT_PARENT="$( cd "$( dirname "$0" )" && pwd )"
+export PYTHONPATH=$SCRIPT_PARENT/Chiplotle-0.3.0-py2.7.egg:$PYTHON_PATH
+PYTHON=python
+
 
 # calibrated from test runs
 XSCALE=1.41
@@ -53,11 +79,15 @@ YSCALE=1.41
 #echo $SCRIPT_PARENT
 echo "Using material $MATERIAL_FILE"
 #exit
-inkscape --export-eps=$STEM.eps --export-area-page --export-text-to-path --without-gui $STEM.svg
-pstoedit -f "hpgl:-pencolors 7 -pencolortable \"#000000,#ff0000,#00ff00,#ffff00,#0000ff,#ff00ff,#00ffff\"" -xscale $XSCALE -yscale $YSCALE $STEM.eps $STEM.plt
-$SCRIPT_PARENT/tidyhpgl4cutter.py $STEM.plt $STEM.clean.plt
+$INKSCAPE --export-eps=$STEM.eps --export-area-page --export-text-to-path --without-gui $STEM.svg
+$PSTOEDIT -f "hpgl:-pencolors 7 -pencolortable \"#000000,#ff0000,#00ff00,#ffff00,#0000ff,#ff00ff,#00ffff\"" -xscale $XSCALE -yscale $YSCALE $STEM.eps $STEM.plt
+$PYTHON $SCRIPT_PARENT/tidyhpgl4cutter.py $STEM.plt $STEM.clean.plt
 # display a simulated plot - map hp2xx pen colours to default Spirit GX colours
 # (actually we should parse the .SGX file to extract the RGB colours)
-hp2xx -c1237465 $STEM.clean.plt &
-$SCRIPT_PARENT/hpgl2cutter.py "$MATERIAL_FILE" $STEM.clean.plt $STEM.clean.pcl
+$HP2XX -c1237465 $STEM.clean.plt &
+$PYTHON $SCRIPT_PARENT/hpgl2cutter.py "$MATERIAL_FILE" $STEM.clean.plt $STEM.clean.pcl
 #hpgl2cutter.py "Laser Settings/Arcmm.SGX" $STEM.plt $STEM.pcl
+
+if [ $PSTOEDIT_NEW_ENOUGH -ne 1 ]; then
+  echo "REPEAT WARNING: laser power settings incorrect due to out-of-date pstoedit (see above)"
+fi
